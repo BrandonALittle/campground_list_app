9b9d7a7e4e76b0f26184638b17dd84c3ca04defa
Brandon Little <brandon.a.little@gmail.com>
2019-01-11 08:46:28 -0500

    setup webpack w/ babel; install react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = "List O' Campgrounds";
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  }
+};
